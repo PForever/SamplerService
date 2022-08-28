caed6ba91ef056da980d74f57a4c276ed7c6009e
Igor Ganov <igor_ganov@live.ru>
2022-08-29 01:53:53 +0300

    Add project files.

---
+++ Sampler.cs
@@ -0,0 +1,31 @@
+namespace SamplerService;
+
+using System.Net;
+
+interface ISampler{
+    Task<SampleResult> SampleAsync(CancellationToken token);
+}
+class Sampler : ISampler
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    public Sampler(IHttpClientFactory httpClientFactory){
+        _httpClientFactory = httpClientFactory;
+    }
+    public async Task<SampleResult> SampleAsync(CancellationToken token){
+        var httpClient = _httpClientFactory.CreateClient(HttpClientNames.VisaTimetable);
+        var httpResponseMessage = await httpClient.GetAsync(
+            "vcs/get_nearest.htm?center=11&persons=&urgent=0&lang=ru", token);
+
+        if (httpResponseMessage.IsSuccessStatusCode)
+        {
+            using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync(token);
+            using var reader = new StreamReader(contentStream);
+            var content = await reader.ReadToEndAsync();
+            
+            return new(content, HasRegistration(content), httpResponseMessage.StatusCode);
+        }
+        return new(null, null, httpResponseMessage.StatusCode);
+    }
+    private static bool HasRegistration(string rowResult) => rowResult == "На ближайшие 2 недели записи нет";
+}
+public readonly record struct SampleResult(string? RowResult, bool? hasRegistration, HttpStatusCode StatusCode);
