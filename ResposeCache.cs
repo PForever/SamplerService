a06b678c6afb30cdfb7b691edb876301d79e1906
Igor Ganov <igor_ganov@live.ru>
2022-08-30 01:12:56 +0300

    push notify only if response changed

---
+++ ResposeCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamplerService;
+
+public interface IResposeCache
+{
+    SampleResult CashedRespose { get; set; }
+}
+
+public class ResposeCache : IResposeCache
+{
+    public SampleResult CashedRespose { get; set; }
+}
